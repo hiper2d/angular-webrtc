8997bb56b72327f975aeac9ca5683500e1f7d958
Alexey Zelenovsky <hiper2d@gmail.com>
2018-12-18 22:38:57 -0500

    Added channel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 
 const ICE_SERVERS: RTCIceServer[] = [
-  {urls: 'stun:stun.services.mozilla.com'},
+  {urls: ['stun:stun.example.com', 'stun:stun-1.example.com']},
   {urls: 'stun:stun.l.google.com:19302'}
 ];
 
@@ -23,6 +23,7 @@ export class AppComponent implements OnInit {
 
   private peerConnection: RTCPeerConnection;
   private signalingConnection: WebSocket;
+  private sendChannel: RTCDataChannel;
   private uuid;
 
   constructor(fb: FormBuilder) {
@@ -35,44 +36,55 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.dataChannelSend.nativeElement.placeholder = 'Press Start, enter some text, then press Send...';
     this.uuid = this.createUuid();
-    this.setupSignalingServer();
-    this.setupPeerServer();
   }
 
   start() {
+    this.setupSignalingServer();
+    this.setupPeerServer();
+
     this.peerConnection
       .createOffer()
-      .then(this.createdDescription())
+      .then(this.setDescription())
       .catch(this.errorHandler);
+  }
+
+  send() {
+    this.sendChannel.send('hi');
   }
 
   private setupSignalingServer() {
     this.signalingConnection = new WebSocket(`ws://${window.location.hostname}:8080/ws/echo`);
-    this.signalingConnection.onmessage = this.getSocketMessageCallback();
+    this.signalingConnection.onmessage = this.getSignalMessageCallback();
+    this.signalingConnection.onerror = this.errorHandler;
   }
 
   private setupPeerServer() {
     this.peerConnection = new RTCPeerConnection(PEER_CONNECTION_CONFIG);
     this.peerConnection.onicecandidate = this.getIceCandidateCallback();
+    // this.peerConnection.ondatachannel = (event) => { console.log(`received message from channel`); };
+    // this.sendChannel = this.peerConnection.createDataChannel('sendDataChannel');
   }
 
-  private getSocketMessageCallback(): (string) => void {
+  private getSignalMessageCallback(): (string) => void {
     return (message) => {
       const signal = JSON.parse(message.data);
-      console.log(`${this.uuid}: received ${signal}`);
-
       if (signal.uuid === this.uuid) {
-        console.log(`${this.uuid}: self`);
         return;
       }
 
+      console.log('Received signal');
+      console.log(signal);
+
       if (signal.sdp) {
-        this.peerConnection.setRemoteDescription(new RTCSessionDescription(signal.sdp)).then(() => {
-          // Only create answers in response to offers
-          if (signal.sdp.type === 'offer') {
-            this.peerConnection.createAnswer().then(this.createdDescription()).catch(this.errorHandler);
-          }
-        }).catch(this.errorHandler);
+        this.peerConnection.setRemoteDescription(new RTCSessionDescription(signal.sdp))
+          .then(() => {
+            if (signal.sdp.type === 'offer') {
+              this.peerConnection.createAnswer()
+                .then(this.setDescription())
+                .catch(this.errorHandler);
+            }
+          })
+          .catch(this.errorHandler);
       } else if (signal.ice) {
         this.peerConnection.addIceCandidate(new RTCIceCandidate(signal.ice)).catch(this.errorHandler);
       }
@@ -81,7 +93,8 @@ export class AppComponent implements OnInit {
 
   private getIceCandidateCallback(): (string) => void {
     return (event) => {
-      console.log('got ice');
+      console.log(`got ice candidate:`);
+      console.log(event);
 
       if (event.candidate != null) {
         this.signalingConnection.send(JSON.stringify({ 'ice': event.candidate, 'uuid': this.uuid }));
@@ -89,13 +102,16 @@ export class AppComponent implements OnInit {
     };
   }
 
-  private createdDescription(): (string) => void {
+  private setDescription(): (string) => void {
     return (description) => {
       console.log('got description');
+      console.log(description);
 
-      this.peerConnection.setLocalDescription(description).then(() => {
-        this.signalingConnection.send(JSON.stringify({ 'sdp': this.peerConnection.localDescription, 'uuid': this.uuid }));
-      }).catch(this.errorHandler);
+      this.peerConnection.setLocalDescription(description)
+        .then(() => {
+          this.signalingConnection.send(JSON.stringify({ 'sdp': this.peerConnection.localDescription, 'uuid': this.uuid }));
+        })
+        .catch(this.errorHandler);
     };
   }
 
